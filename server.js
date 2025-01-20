7553206feefb19109186ef79f408766d9e93a703
SimonITHS <simon.kovac@iths.se>
2025-01-20 13:02:09 +0100

    test

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ console.log("Hello World!");
 const express = require("express");
 const app = express();
 const PORT = 3000;
-
-
-app.listen(PORT, () => {
-    console.log("listening to 3000");
+const mongoose = require("mongoose");
+app.get("/", (req, res) => {
+    res.send("welcome to my mongoAPI")
 });
 
+app.post("/blogpost", (req, res) => {
+    res.send("this is a postrequest")
+});
+
+mongoose.
+connect("mongodb+srv://simon:123admin@simonsapi.ebu7p.mongodb.net/products-API?retryWrites=true&w=majority&appName=SimonsApi")
+.then(() => {
+    console.log("connected to mongo");
+    app.listen(PORT, () => {
+        console.log("listening to meeee");
+    });
+});
