4279a00df85fb8ddb0b40d221ceafdc780363722
SimonITHS <simon.kovac@iths.se>
2025-01-20 10:37:55 +0100

    hej

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 console.log("Hello World!");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+    console.log("listening to 3000");
+});
+
